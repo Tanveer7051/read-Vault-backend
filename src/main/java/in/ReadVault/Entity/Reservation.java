60f0093e7efc4397efc8629d6e6267fd6f3adddb
Tanveer Ahmed <tanveerahmedhajam7051@gmail.com>
2026-04-30 22:36:33 +0530

    Updated Enities

--- src/main/java/in/ReadVault/Entity/Reservation.java
+++ src/main/java/in/ReadVault/Entity/Reservation.java
@@ -1,13 +1,48 @@
 package in.ReadVault.Entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDate createdDate;
+
+    @Column(nullable = false)
+    private LocalDate expiryDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReservationStatus status = ReservationStatus.PENDING;
+
+    @Column(nullable = false)
+    private Integer queuePosition;
+
+    @JsonBackReference(value = "user-reservation")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @JsonBackReference(value = "book-reservation")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdDate = LocalDate.now();
+    }
 }
