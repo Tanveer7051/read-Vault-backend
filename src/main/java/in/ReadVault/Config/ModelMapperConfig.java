a050b7d3231d09ad4240a980fa720889e0762c64
Tanveer Ahmed <tanveerahmedhajam7051@gmail.com>
2026-05-01 00:07:46 +0530

    Add ModelMapper configuration for DTO and entity mapping

--- src/main/java/in/ReadVault/Config/ModelMapperConfig.java
+++ src/main/java/in/ReadVault/Config/ModelMapperConfig.java
@@ -1,5 +1,13 @@
 package in.ReadVault.Config;
 
-public class ModelMapperConfig {
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper getModelMapper(){
+        return  new ModelMapper();
+    }
 }
