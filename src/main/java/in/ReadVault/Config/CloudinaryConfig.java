2a89c86736b241b8eb220a23dd483a7f40b08278
Tanveer Ahmed <tanveerahmedhajam7051@gmail.com>
2026-05-07 15:04:33 +0530

    updated CloudinaryConfig hide the sensitive data

--- src/main/java/in/ReadVault/Config/CloudinaryConfig.java
+++ src/main/java/in/ReadVault/Config/CloudinaryConfig.java
@@ -1,6 +1,8 @@
 package in.ReadVault.Config;
 
+
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +12,23 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
+
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dvm5udmdz");
-        config.put("api_key", "872983257814465");
-        config.put("api_secret", "F45h1nAlEMXK9Ea2YYj_Tt46k5c");
+
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
 
         return new Cloudinary(config);
     }
