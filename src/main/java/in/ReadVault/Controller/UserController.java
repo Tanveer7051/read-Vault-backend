f03a86b8c068e52f0c2c4f5cee902c0ef1373b77
Tanveer Ahmed <tanveerahmedhajam7051@gmail.com>
2026-05-07 15:03:28 +0530

    updated controller, now user can update their profile

--- src/main/java/in/ReadVault/Controller/UserController.java
+++ src/main/java/in/ReadVault/Controller/UserController.java
@@ -1,11 +1,18 @@
 package in.ReadVault.Controller;
 
+import in.ReadVault.DTO.UpdateUserDTO;
 import in.ReadVault.DTO.UserDTO;
+import in.ReadVault.Entity.User;
 import in.ReadVault.Service.UserService;
-import org.modelmapper.ModelMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import tools.jackson.databind.ObjectMapper;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,30 +20,56 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController {
+
     private final UserService userService;
 
-    public UserController(UserService userService, ModelMapper modelMapper) {
-        this.userService = userService;
-    }
-
     @GetMapping("/all")
-    public List<UserDTO> getAllUser(){
+    public List<UserDTO> getAllUser() {
         return userService.getAllUser();
     }
 
     @GetMapping("/{userId}")
-    public UserDTO getUser(@PathVariable Long userId){
+    public UserDTO getUser(@PathVariable Long userId) {
         return userService.getUser(userId);
+    }
+    @PutMapping("/update")
+    public ResponseEntity<Map<String, Object>> updateUser(
+
+            Authentication authentication,
+
+            @Valid @ModelAttribute UpdateUserDTO updateUserDTO,
+
+            @RequestParam(value = "image", required = false)
+            MultipartFile image
+    ) {
+
+        Long userId =
+                ((User) authentication.getPrincipal()).getId();
+
+        UserDTO updatedUser =
+                userService.updateUser(userId, updateUserDTO, image);
+
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+                "message", "User Successfully Updated",
+                "status", HttpStatus.OK,
+                "date", LocalDate.now(),
+                "user", updatedUser
+        ));
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<Map<String, Object>> deleteUser(@PathVariable Long userId){
+    public ResponseEntity<Map<String, Object>> deleteUser(
+            @PathVariable Long userId
+    ) {
+
         userService.deleteUser(userId);
+
         return ResponseEntity.status(HttpStatus.OK).body(Map.of(
                 "userId", userId,
                 "message", "User Successfully Deleted",
-                "status" , HttpStatus.OK,
+                "status", HttpStatus.OK,
                 "date", LocalDate.now()
         ));
     }
