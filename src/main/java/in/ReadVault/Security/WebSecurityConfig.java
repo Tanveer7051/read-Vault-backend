1b471b19d340b053e24f728af859d1addb4e73c8
Tanveer Ahmed <tanveerahmedhajam7051@gmail.com>
2026-05-07 11:42:20 +0530

    Added Complete Security

--- src/main/java/in/ReadVault/Security/WebSecurityConfig.java
+++ src/main/java/in/ReadVault/Security/WebSecurityConfig.java
@@ -1,5 +1,74 @@
 package in.ReadVault.Security;
 
+import in.ReadVault.Filter.JwtFilter;
+import in.ReadVault.GlobalExceptionHandling.CustomAccessDeniedHandler;
+import in.ReadVault.GlobalExceptionHandling.JwtAuthEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .exceptionHandling(exception -> exception
+                        .accessDeniedHandler(new CustomAccessDeniedHandler())
+                        .authenticationEntryPoint(new JwtAuthEntryPoint()))
+
+                .authorizeHttpRequests(auth -> auth
+
+                                // PUBLIC
+                                .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/user/all").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/user/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/user/**").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PUT, "/api/user/**").hasAnyRole("ADMIN", "USER")
+
+                                // BOOKS
+                        .requestMatchers(HttpMethod.POST, "/api/book/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/book/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/book/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/book/**").hasAnyRole("ADMIN", "USER")
+
+                                // BORROW
+                        .requestMatchers(HttpMethod.POST, "/api/borrow/take/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/api/borrow/digital/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.PUT, "/api/borrow/return/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/borrow/**").hasAnyRole("ADMIN", "USER")
+
+
+                        .requestMatchers(HttpMethod.POST, "/api/reservations/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/api/reservations").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/reservations/**").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.PUT, "/api/reservations/*/status").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/reservations/*/cancel").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/reservations/**").hasRole("ADMIN")
+
+                                // ANY OTHER
+                                .anyRequest().authenticated()
+                )
+
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
