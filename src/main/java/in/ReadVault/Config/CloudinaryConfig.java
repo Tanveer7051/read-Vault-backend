ec775777de390b0164400366bc24b1af4a09e2de
Tanveer Ahmed <tanveerahmedhajam7051@gmail.com>
2026-05-07 11:48:24 +0530

    Initial complete version of ReadVault

--- src/main/java/in/ReadVault/Config/CloudinaryConfig.java
+++ src/main/java/in/ReadVault/Config/CloudinaryConfig.java
@@ -1,4 +1,22 @@
 package in.ReadVault.Config;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class CloudinaryConfig {
 
+    @Bean
+    public Cloudinary cloudinary() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", "dvm5udmdz");
+        config.put("api_key", "872983257814465");
+        config.put("api_secret", "F45h1nAlEMXK9Ea2YYj_Tt46k5c");
+
+        return new Cloudinary(config);
+    }
 }
--- src/main/java/in/ReadVault/ReadVaultApplication.java
+++ src/main/java/in/ReadVault/ReadVaultApplication.java
@@ -2,8 +2,10 @@ package in.ReadVault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ReadVaultApplication {
 
 	public static void main(String[] args) {
