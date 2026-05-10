e25d64e74643bcf3f511abba16d2f9439539171a
Tanveer Ahmed <tanveerahmedhajam7051@gmail.com>
2026-05-10 17:46:40 +0530

    Update AppConfig

--- src/main/java/in/ReadVault/Config/AppConfig.java
+++ src/main/java/in/ReadVault/Config/AppConfig.java
@@ -1,22 +1,43 @@
 package in.ReadVault.Config;
 
+
+import in.ReadVault.Service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
+    private final UserService userService;
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration){
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception {
+
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider(userService);
+
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
     }
 }
